2071fc6a3aac59a635c23f53a3441cbc859c0868
Radiaxx <110401982+Radiaxx@users.noreply.github.com>
2024-02-17 10:00:59 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -8,32 +8,32 @@ var path = require('path');
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
-  var uri = url.parse(request.url).pathname;
-  var filename = path.join(process.cwd(), uri);
+    var uri = url.parse(request.url).pathname;
+    var filename = path.join(process.cwd(), uri);
 
-  fs.exists(filename, function(exists) {
-    if (!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
-      response.end();
-      return;
-    }
+    fs.access(filename, fs.constants.F_OK, function(err) {
+        if (err) {
+            response.writeHead(404, {"Content-Type": "text/plain"});
+            response.write("404 Not Found\n");
+            response.end();
+            return;
+        }
 
-    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+        if (fs.statSync(filename).isDirectory()) filename += '/index.html';
 
-    fs.readFile(filename, "binary", function(err, file) {
-      if (err) {
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
+        fs.readFile(filename, "binary", function(err, file) {
+            if (err) {
+                response.writeHead(500, {"Content-Type": "text/plain"});
+                response.write(err + "\n");
+                response.end();
+                return;
+            }
 
-      response.writeHead(200);
-      response.write(file, "binary");
-      response.end();
+            response.writeHead(200);
+            response.write(file, "binary");
+            response.end();
+        });
     });
-  });
 });
 
 // Listen on port 8000, IP defaults to
